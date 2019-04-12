0491eac5c491ba50dc6bed543cf3fc92c3660cc1
Ashley20 <kucukkaraaslan.didem@gmail.com>
2019-04-12 17:59:38 +0300

    Login functionality with error handling implemented

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { withFirebase } from "react-redux-firebase";
 import { Link } from "react-router-dom";
 import {
   Grid,
@@ -14,7 +18,9 @@ import "../../app.css";
 class Login extends Component {
   state = {
     email: "",
-    password: ""
+    password: "",
+    loading: false,
+    errors: []
   };
 
   handleChange = event => {
@@ -23,8 +29,48 @@ class Login extends Component {
     });
   };
 
-  handleSubmit = event => {};
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const { email, password } = this.state;
+    this.setState({ loading: true, errors: [] });
+
+    if (this.isFormValid()) {
+      this.props.firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(signedInUser => {
+          console.log(signedInUser);
+          this.setState({ loading: false, errors: [] });
+        })
+        .catch(error => {
+          console.log(error)
+          this.setState(prevState => ({
+            loading: false,
+            errors: [...prevState.errors, error]
+          }));
+        });
+    } else {
+      this.setState({
+        loading: false,
+        errors: [
+          {
+            code: "form_is_empty",
+            message: "Please fill in all the fields."
+          }
+        ]
+      });
+    }
+  };
+
+  isFormValid = () => this.state.email && this.state.password;
+
+  displayErrors = () =>
+    this.state.errors.map((error, key) => <p key={key}>{error.message}</p>);
+
   render() {
+    const { errors } = this.state;
+
     return (
       <div className="app">
         <Grid
@@ -71,6 +117,9 @@ class Login extends Component {
                 </Button>
               </Segment>
             </Form>
+            {errors.length > 0 && (
+              <Message error> {this.displayErrors()}</Message>
+            )}
             <Message>
               <Icon name="help" />
               Dont have an account?&nbsp;
@@ -84,4 +133,11 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  firebase: PropTypes.object
+};
+
+export default compose(
+  withFirebase,
+  connect(({ firebase: { auth } }) => ({ auth }))
+)(Login);
